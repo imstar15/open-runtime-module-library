46b09d327d942b5d8a95d7cad829d4afa3164f33
imstar15 <imstar15@qq.com>
2022-11-11 17:55:44 +0800

    debug

--- xtokens/src/mock/teleport_currency_adapter.rs
+++ xtokens/src/mock/teleport_currency_adapter.rs
@@ -111,7 +111,7 @@ impl<
 	}
 
 	fn withdraw_asset(asset: &MultiAsset, location: &MultiLocation) -> result::Result<Assets, XcmError> {
-		log::error("CurrencyIdConversionFailed!!!withdraw_asset");
+		log::error!("CurrencyIdConversionFailed!!!withdraw_asset");
 		UnknownAsset::withdraw(asset, location).or_else(|_| {
 			let who = AccountIdConvert::convert_ref(location)
 				.map_err(|_| XcmError::from(Error::AccountIdConversionFailed))?;
