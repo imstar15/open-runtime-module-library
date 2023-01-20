56edaf274cd769edc05f599356e45c0fa2734119
imstar15 <imstar15@qq.com>
2023-01-21 00:44:28 +0800

    Debug

--- tokens/src/lib.rs
+++ tokens/src/lib.rs
@@ -719,7 +719,7 @@ impl<T: Config> Pallet<T> {
 		}
 
 		log::error!("BalanceTooLow tokens ensure_can_withdraw currency_id: {:?}, who: {:?}, amount: {:?}", currency_id, who, amount);
-		const free_balance = Self::free_balance(currency_id, who);
+		let free_balance = Self::free_balance(currency_id, who);
 		log::error!("BalanceTooLow tokens ensure_can_withdraw free_balance: {:?}", free_balance);
 		let new_balance = free_balance.checked_sub(&amount)
 			.ok_or(Error::<T>::BalanceTooLow)?;
